e67df5ba79f428c8822c2fb16091a3e6b79fcf0a
samsun <samsun639@163.com>
2016-12-30 11:17:31 +0800

    Create adb_push.sh
    use after mm, to push generated so to the phone

---
+++ adb_push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+adb root
+adb remount rw
+
+#cd $OUT
+
+while read line;
+do 
+	SOURCE=`echo $line | sed -n  's/Install.*\(\/system\)/\1/p'`
+	TO=`echo ${SOURCE}|awk -F"/" '{for(i=1;i<NF;i++) printf $i"/"}'`
+	adb push .${SOURCE} ${TO}
+	#echo "adb push .${SOURCE} ${TO}"
+	#echo "adb push .${SOURCE} ${SOURCE}"
+done < $1
+
