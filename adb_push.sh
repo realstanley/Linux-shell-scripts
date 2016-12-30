d2c5149e854ad8aca36b6fd8e2e171c2386777a5
samsun <samsun639@163.com>
2016-12-30 11:32:17 +0800

    Update adb_push.sh

--- adb_push.sh
+++ adb_push.sh
@@ -8,8 +8,7 @@ while read line;
 do 
 	SOURCE=`echo $line | sed -n  's/Install.*\(\/system\)/\1/p'`
 	TO=`echo ${SOURCE}|awk -F"/" '{for(i=1;i<NF;i++) printf $i"/"}'`
+	echo "adb push .${SOURCE} ${TO}"
 	adb push .${SOURCE} ${TO}
-	#echo "adb push .${SOURCE} ${TO}"
 	#echo "adb push .${SOURCE} ${SOURCE}"
 done < $1
-
